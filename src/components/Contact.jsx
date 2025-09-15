fdedbb12769b6a75d96e9f774b13a3e5c1b09ece
nikhilagatadi <166213324+nikhilagatadi@users.noreply.github.com>
2025-09-15 14:43:18 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ export default function Contact() {
       <div className="container text-center">
         <h2>Contact</h2>
         <p>Email: <a href="mailto:gatadinikhila@gmail.com">gatadinikhila@gmail.com</a></p>
-        <p>Phone: <a href="tel:+919346153191">+91 9346153191</a></p>
+        <p>Phone: <a href="tel:+919346153191">+91 9193461531</a></p>
       </div>
     </section>
   )
